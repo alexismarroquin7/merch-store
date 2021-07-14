7c92fe4b3542ca77bf90c66d25eb3c424227d943
alexismarroquin7 <alexismarroquin754@gmail.com>
2021-07-14 15:23:43 -0700

    categories seed data

---
+++ data/sample-data/categories.js
@@ -0,0 +1,16 @@
+module.exports = [
+  { 
+    category_name: 'hoodie',
+    category_description: ''
+  },
+  { 
+    category_name: 'hats',
+    category_description: ''
+  },
+  { 
+    category_name: 'shirts'
+  },
+  { 
+    category_name: 'bracelets'
+  },
+];

b7cfeeeaccf2efd5bf3f21dc1e578aee23d95d95
alexismarroquin7 <alexismarroquin754@gmail.com>
2021-07-18 16:44:54 -0700

    adds images tables for unique items

--- api/data/migrations/20210716121605_second-migration.js
+++ api/data/migrations/20210716121605_second-migration.js
@@ -3,23 +3,48 @@ exports.up = (knex) => {
   return knex.schema
   .createTable('sizes', sizes => {
     sizes.increments('size_id');
+    
     sizes.string('size_name')
       .notNullable()
-      .unique();
+      .unique();  
+    
     sizes.string('size_description');
+  
   })
+  
   .createTable('colors', colors => {
     colors.increments('color_id');
+  
     colors.string('color_name')
       .notNullable()
       .unique();
+  
     colors.string('color_description');
+
   })
+  
+  .createTable('images', images => {
+    images.increments('image_id');
+    
+    images.string('image_name')
+      .notNullable()
+      .unique();
+    
+    images.string('image_src')
+      .notNullable()
+      .unique();
+    
+    images.string('image_description');
+  })
+  
   .createTable('inventory', inventory => {
     inventory.increments('inventory_id');
+    
     inventory.integer('quantity');
+    
     inventory.timestamp('created_at')
       .defaultTo(knex.fn.now());
+    
     inventory.integer('product_id')
       .unsigned()
       .notNullable()
@@ -27,6 +52,7 @@ exports.up = (knex) => {
       .inTable('products')
       .onDelete('RESTRICT')
       .onUpdate('CASCADE');
+  
     inventory.integer('size_id')
       .unsigned()
       .notNullable()
@@ -34,11 +60,33 @@ exports.up = (knex) => {
       .inTable('sizes')
       .onDelete('RESTRICT')
       .onUpdate('CASCADE');
+
     inventory.integer('color_id')
       .unsigned()
       .notNullable()
       .references('color_id')
       .inTable('colors')
+      .onDelete('RESTRICT')
+      .onUpdate('CASCADE');
+  })
+  
+  .createTable('inventory_images', inventory_images => {
+    inventory_images.increments('inventory_image_id');
+    
+    inventory_images.integer('image_id')
+      .unsigned()
+      .unique()
+      .notNullable()
+      .references('image_id')
+      .inTable('images')
+      .onDelete('RESTRICT')
+      .onUpdate('CASCADE');
+    
+    inventory_images.integer('inventory_id')
+      .unsigned()
+      .notNullable()
+      .references('inventory_id')
+      .inTable('inventory')
       .onDelete('RESTRICT')
       .onUpdate('CASCADE');
   });
